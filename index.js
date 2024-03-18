42d4ae3b3a79e70d6fd4e7cabb4e7a693d933fd3
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-03-18 18:45:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 2
 
-// Write your code here
+let num2 = 31
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() + 1)
+
+let num3 = 25
+let num4 = 7
+let mod = 25 % 7
+
+const num = (5,10,15,20);
+const max = Math.max(num);
+let infinity = 20
+console.log(infinity);
+
